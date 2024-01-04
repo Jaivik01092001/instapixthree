e7553cddba9e56234e4ed829d73516c8d6c2441c
Patel Jaivik <120545678+Jaivik01092001@users.noreply.github.com>
2024-01-04 13:03:00 +0530

    fix horizontal

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -81,72 +81,74 @@ const Footer = () => {
               </div>
             </motion.div>
           </div>
-          <motion.div
-            initial={{ opacity: 0, x: 100 }}
-            whileInView={{ opacity: 1, x: 0 }}
-            transition={{ duration: 1, ease: "easeOut" }}
-            viewport={{ once: true }}
-            className="frcoloumntwo p-8 "
-          >
-            <div className="nameemail flex">
-              <div className="name flex flex-col">
-                <label htmlFor="fullname ">
-                  Full Name<sup>*</sup>
+          <div className="overflow-x-hidden">
+            <motion.div
+              initial={{ opacity: 0, x: 100 }}
+              whileInView={{ opacity: 1, x: 0 }}
+              transition={{ duration: 1, ease: "easeOut" }}
+              viewport={{ once: true }}
+              className="frcoloumntwo p-8 "
+            >
+              <div className="nameemail flex">
+                <div className="name flex flex-col">
+                  <label htmlFor="fullname ">
+                    Full Name<sup>*</sup>
+                  </label>
+                  <input
+                    type="text"
+                    placeholder="John David"
+                    autoComplete="off"
+                  />
+                </div>
+                <div className="email flex flex-col">
+                  <label htmlFor="fullemail">
+                    Your Email<sup>*</sup>
+                  </label>
+                  <input
+                    type="email"
+                    placeholder="example@yourmail.com"
+                    autoComplete="off"
+                  />
+                </div>
+              </div>
+              <div className="phonesubject flex ">
+                <div className="phoneform flex flex-col">
+                  <label htmlFor="fullphone">
+                    Phone<sup>*</sup>
+                  </label>
+                  <input
+                    type="text"
+                    placeholder="(000) 123 456"
+                    autoComplete="off"
+                  />
+                </div>
+                <div className="project flex flex-col">
+                  <label htmlFor="fullproject">
+                    Your Email<sup>*</sup>
+                  </label>
+                  <input
+                    type="text"
+                    placeholder="ex. project "
+                    autoComplete="off"
+                  />
+                </div>
+              </div>
+              <div className="leaveusmessage flex flex-col">
+                <label htmlFor="leaveus">
+                  Leave Us A Message <sup>*</sup>
                 </label>
                 <input
                   type="text"
-                  placeholder="John David"
+                  className="message"
+                  placeholder="Hello there,I would like to talk about how to..."
                   autoComplete="off"
                 />
               </div>
-              <div className="email flex flex-col">
-                <label htmlFor="fullemail">
-                  Your Email<sup>*</sup>
-                </label>
-                <input
-                  type="email"
-                  placeholder="example@yourmail.com"
-                  autoComplete="off"
-                />
+              <div className="sendbtn">
+                <button className="sendtext">Send Message</button>
               </div>
-            </div>
-            <div className="phonesubject flex ">
-              <div className="phoneform flex flex-col">
-                <label htmlFor="fullphone">
-                  Phone<sup>*</sup>
-                </label>
-                <input
-                  type="text"
-                  placeholder="(000) 123 456"
-                  autoComplete="off"
-                />
-              </div>
-              <div className="project flex flex-col">
-                <label htmlFor="fullproject">
-                  Your Email<sup>*</sup>
-                </label>
-                <input
-                  type="text"
-                  placeholder="ex. project "
-                  autoComplete="off"
-                />
-              </div>
-            </div>
-            <div className="leaveusmessage flex flex-col">
-              <label htmlFor="leaveus">
-                Leave Us A Message <sup>*</sup>
-              </label>
-              <input
-                type="text"
-                className="message"
-                placeholder="Hello there,I would like to talk about how to..."
-                autoComplete="off"
-              />
-            </div>
-            <div className="sendbtn">
-              <button className="sendtext">Send Message</button>
-            </div>
-          </motion.div>
+            </motion.div>
+          </div>
         </div>
         <div className="hr m-auto my-16">
           <hr />
