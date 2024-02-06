921027306a8092375458d7b0c8f5dfcaff8b4f13
Patel Jaivik <jaivik.patel@fabaf.in>
2024-02-06 09:48:36 +0530

    keyfeature and hit section

--- src/Components/HowItWork/HIT.jsx
+++ src/Components/HowItWork/HIT.jsx
@@ -7,6 +7,32 @@ import hdr from "../../Assets/receivehdrphoto.png";
 import arrow from "../../Assets/arrow.png";
 import { motion } from "framer-motion";
 
+const HITSteps = [
+  {
+    title: "Find Photographers",
+    description:
+      "Search Our Nation Wide Marketplace Of Qualified Real Estate Photographers using Our Ai Powered Capture And Editing Solution.",
+    ctaText: "Get Started",
+    image: find,
+    arrow: arrow,
+  },
+  {
+    title: "Book Photographers",
+    description:
+      "Instantly Bok An Appoinment With Your Desired Photographer      ",
+    ctaText: "Get Started",
+    image: book,
+    arrow: arrow,
+  },
+  {
+    title: "Received HDR Photos    ",
+    description:
+      "With Our Fast, Efficient, Service, Photo Are Delivered On Site, Saving Time And Money While Showcasing The Property In The Best Possible Light      ",
+    ctaText: "Get Started",
+    image: hdr,
+    arrow: arrow,
+  },
+];
 const HIT = () => {
   return (
     <>
@@ -22,88 +48,36 @@ const HIT = () => {
             How It Works
           </motion.div>
         </div>
-        <div className="hitbottom flex flex-col  ">
-          <div className="flex contentcontainer">
-            <div className="leftmockup overflow-x-hidden">
-              <motion.img
-                initial={{ opacity: 0, x: -100 }}
-                whileInView={{ opacity: 1, x: 0 }}
-                transition={{ duration: 1, ease: "easeOut" }}
-                viewport={{ once: true }}
-                className="leftmockupimg"
-                src={hitmockup}
-                alt=""
-              />
+        <div className="hitbottomcontainer flex">
+          <div className="layoutsetting">
+            <div className="left">
+              <img src={hitmockup} alt="" />
             </div>
-
-            <div className="righthit overflow-x-hidden">
-              <motion.div
-                initial={{ opacity: 0, x: 100 }}
-                whileInView={{ opacity: 1, x: 0 }}
-                transition={{ duration: 1, ease: "easeOut" }}
-                viewport={{ once: true }}
-                className="righthitone"
-              >
-                <div className="imgiconone">
-                  <img className="imgiconset" src={find} alt="" />
-                </div>
-                <div className="hittitle ani">Find Photographers</div>
-                <div className="hitdesc">
-                  Search our nation wide marketplace of qualified real estate
-                  photographerscusing our ai powered capture and editing
-                  solution.
-                </div>
-                <div className="hitcta flex mt-8 ">
-                  <div className="ctatext">Get Started</div>
-                  <img className="ml-4" src={arrow} alt="" />
-                </div>
-              </motion.div>
+            <div className="right mt-80">
+              <div className="content">
+                {HITSteps.map((step, index) => (
+                  <div key={index} className="findphototgraphercontainer mb-72">
+                    <div className="bg-blue-200 w-fit p-4 rounded-md max-h-20 max-w-20">
+                      <img
+                        className="max-w-[80%]"
+                        src={step.image}
+                        alt={step.title}
+                      />
+                    </div>
+                    <div className="hittitle">{step.title}</div>
+                    <div className="hitdesc">{step.description}</div>
+                    <div className="hitcta flex items-center mt-8">
+                      <div className="ctatext">{step.ctaText}</div>
+                      <img
+                        className="ml-4 object-contain"
+                        src={step.arrow}
+                        alt=""
+                      />
+                    </div>
+                  </div>
+                ))}
+              </div>
             </div>
-          </div>
-
-          <div className="righthitsecond overflow-x-hidden">
-            <motion.div
-              initial={{ opacity: 0, x: 100 }}
-              whileInView={{ opacity: 1, x: 0 }}
-              transition={{ duration: 1, ease: "easeOut" }}
-              viewport={{ once: true }}
-              className="righthitone"
-            >
-              <div className="imgicontwo">
-                <img className="imgiconset" src={book} alt="" />
-              </div>
-              <div className="hittitle">Book Photographers</div>
-              <div className="hitdesc">
-                Instantly bok an Appoinment with your desired photographer
-              </div>
-              <div className="hitcta flex mt-8 ">
-                <div className="ctatext">Get Started</div>
-                <img className="ml-4 object-contain" src={arrow} alt="" />
-              </div>
-            </motion.div>
-          </div>
-          <div className="righthitthird overflow-hidden">
-            <motion.div
-              initial={{ opacity: 0, x: 100 }}
-              whileInView={{ opacity: 1, x: 0 }}
-              transition={{ duration: 1, ease: "easeOut" }}
-              viewport={{ once: true }}
-              className="righthitone"
-            >
-              <div className="imgiconthree">
-                <img className="imgiconset" src={hdr} alt="" />
-              </div>
-              <div className="hittitle">Received HDR photos</div>
-              <div className="hitdesc">
-                With our fast, efficient, service, photo are delivered on site,
-                saving time and money while showcasing the property in the best
-                possible light
-              </div>
-              <div className="hitcta flex mt-8 ">
-                <div className="ctatext">Get Started</div>
-                <img className="ml-4 object-contain" src={arrow} alt="" />
-              </div>
-            </motion.div>
           </div>
         </div>
       </div>
--- src/Components/featureofinstapix/Featureofinstapix.jsx
+++ src/Components/featureofinstapix/Featureofinstapix.jsx
@@ -1,167 +1,156 @@
 import React, { useState } from "react";
 import "../featureofinstapix/Featureofinstapix.css";
-import skyreplacement from "../../Assets/2nd design assets/skyreplacement.svg";
-import { motion } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
+import beforebutton from "../../Assets/beforebutton.svg";
+import afterbutton from "../../Assets/afterbutton.svg";
+
+import ReactCompareImage from "react-compare-image";
+import beforeimgskyreplace from "../../Assets/Features Comparison Images/Sky Replace-Before.jpg";
+import afterimageskyreplace from "../../Assets/Features Comparison Images/Sky Replace-After.jpg";
+import beforehdr from "../../Assets/Features Comparison Images/HDR-Before.JPG";
+import afterhdr from "../../Assets/Features Comparison Images/HDR-After.jpg";
+
+import beforimagesaturation from "../../Assets/Features Comparison Images/Saturation-Before.jpg";
+import afterimagesaturation from "../../Assets/Features Comparison Images/Saturation-After.jpg";
+import beforecontrast from "../../Assets/Features Comparison Images/Contrast-Before.jpg";
+import aftercontrast from "../../Assets/Features Comparison Images/Contrast-After.jpg";
+import beforeimgrelightning from "../../Assets/Features Comparison Images/Image Relighting-Before.jpg";
+import afterimgrelightning from "../../Assets/Features Comparison Images/Image Relighting-After.jpg";
+import beforePerspectivecorrection from "../../Assets/Features Comparison Images/Perspective Correction-Before.jpg";
+import afterPerspectivecorrection from "../../Assets/Features Comparison Images/Perspective Correction-After.jpg";
 
 const Featureofinstapix = () => {
   const features = [
-    "Sky replacement",
-    "360 Enhancement",
+    "Sky Replacement",
+    // "360 Enhancement",
     "HDR Image",
-    "Perspective correction",
+    "Perspective Correction",
     "Image Relighting",
-    "Sales and Marketing",
-    "Raw and Jpg Format",
-    "Contrast and Brightness boost",
-    "Lens correction",
+    // "Sales and Marketing",
+    // "Raw and Jpg Format",
+    "Contrast Boost",
+    "Brightness Boost",
+    "Lens Correction",
+    "Saturation Level",
+    "Noise Reduction",
+    "Image Sharpening",
   ];
 
-  const [selectedFeature, setSelectedFeature] = useState("Sky replacement");
+  const featureContent = {
+    "Sky Replacement": {
+      title: "Sky replacement",
+      desc: "When an exterior photo is captured, the app's AI technology identifies the sky area and replaces it with a stunning blue sky in realtime. This feature enhances outdoor images, making them look more appealing and vibrant without requiring any manual editing skills. Additionally, users have the option to customize the sky with settings for cloud cover, allowing them to choose from clear skies, low clouds, or high clouds, further enhancing their outdoor images with ease.",
+      leftimage: beforeimgskyreplace,
+      rightimage: afterimageskyreplace,
+    },
+    "360 Enhancement": {
+      title: "360 Enhancement",
+      desc: "Dummy content for 360 Enhancement.",
+    },
+    "HDR Image": {
+      title: "HDR Image",
+      desc: "This mobile application automatically captures 5 high dynamic range (HDR) images and seamlessly processes them in the cloud using advanced AI editing technology. Once the images are captured, they are quickly sent to the cloud, where AI algorithms work their magic. These algorithms optimize exposure, adjust colors, and reduce noise to create stunning, professional-quality photos.",
+      leftimage: beforehdr,
+      rightimage: afterhdr,
+    },
+    "Perspective Correction": {
+      title: "Perspective correction",
+      desc: "This mobile app automatically corrects perspective distortion, ensuring a professional finish by automatically adjusting skewed angles in photos, guaranteeing polished and well-aligned images without any manual intervention.",
+      leftimage: beforePerspectivecorrection,
+      rightimage: afterPerspectivecorrection,
+    },
+    "Image Relighting": {
+      title: "Image Relighting",
+      desc: "The app's advanced AI editing technology excels in Image Relighting, ensuring impeccable contrast and lighting in photos. This powerful feature automatically analyzes each image and intelligently adjusts the lighting conditions to bring out the perfect contrast and illumination.",
+      leftimage: beforeimgrelightning,
+      rightimage: afterimgrelightning,
+    },
+    "Sales and Marketing": {
+      title: "Sales and Marketing",
+      desc: "Dummy content for Sales and Marketing.",
+    },
+    "Raw and Jpg Format": {
+      title: "Raw and Jpg Format",
+      desc: "Dummy content for Raw and Jpg Format.",
+    },
+    "Contrast Boost": {
+      title: "Contrast boost",
+      desc: "The app offers users the flexibility to fine-tune contrast settings according to their preferences. With easy-to-use controls, customers can customize contrast levels to achieve the exact look they desire in their photos. This feature empowers users to have full control over the contrast, allowing them to create images that perfectly match their creative vision.",
+      leftimage: beforecontrast,
+      rightimage: aftercontrast,
+    },
+    "Brightness Boost": {
+      title: "Brightness boost",
+      desc: "With user-friendly controls, customers can tailor the brightness levels to match their specific preferences for each photo. This feature grants users full control over brightness, enabling them to create images that align perfectly with their creative vision and desired aesthetic.",
+    },
+    "Lens Correction": {
+      title: "Lens correction",
+      desc: "The app's AI editing technology goes a step further by automatically correcting lens distortion, effectively eliminating barrel distortion from your photos. Through intelligent analysis, it identifies and rectifies any distortions caused by the camera lens, ensuring that straight lines appear straight and that the image remains true to reality.",
+    },
+    "Saturation Level": {
+      title: "Saturation Level",
+      desc: "With intuitive controls, customers can customize the saturation of their photos to achieve the desired color intensity. This feature provides users with the flexibility to create images that perfectly match their unique style and artistic vision.",
+      leftimage: beforimagesaturation,
+      rightimage: afterimagesaturation,
+    },
+    "Noise Reduction": {
+      title: "Noise Reduction",
+      desc: "The app's AI editing technology also excels in noise reduction, automatically identifying and reducing noise in photos. Through sophisticated algorithms, it effectively cleans up grainy or pixelated elements, resulting in smoother and more refined images.",
+    },
+    "Image Sharpening": {
+      title: "Image Sharpening",
+      desc: "The app's AI editing technology automatically enhances photo sharpness, providing an extra boost in quality. Using advanced algorithms, it intelligently identifies areas that need sharpening, ensuring that every detail in the image is crisp and well-defined.",
+    },
+  };
+
+  const [selectedFeature, setSelectedFeature] = useState("Sky Replacement");
 
   const renderFeatureContent = () => {
-    switch (selectedFeature) {
-      case "Sky replacement":
-        return (
-          <>
-            <div className="menutitle">Sky replacement</div>
-            <div className="menudesc">
-              Completely synergize resource taxing relationships via premier
-              niche markets. Professionally cultivate one-to-one customer
-              service with service for state of the art customer service.
-              Completely synergize resource taxing relationships via premier
-              niche markets. Professionally cultivate one-to-one customer
-              service with service for state of the art customer service.
+    const feature = featureContent[selectedFeature];
+    if (feature) {
+      return (
+        <>
+          <div className="flex gap-40  mobilesetgap">
+            <div className="w-2/4 leftcontsetmobile">
+              <div className="menutitle">{feature.title}</div>
+              <div className="menudesc">{feature.desc}</div>
+              <div className="menudownload w-fit px-20 py-8 rounded-full">
+                Download App
+              </div>
             </div>
-            <div className="menudownload w-fit px-20 py-8 rounded-full">
-              Download App
+            <div className="w-2/4 rightcontsetmobile position-relative ">
+              <ReactCompareImage
+                className="imageset rightcontsetmobile"
+                leftImage={feature.leftimage}
+                rightImage={feature.rightimage}
+              />
+              <img src={beforebutton} className="beforebutton" alt="" />
+              <img src={afterbutton} className="afterbutton" alt="" />
             </div>
-          </>
-        );
-      case "360 Enhancement":
-        return (
-          <>
-            <div className="menutitle">360 Enhancement</div>
-            <div className="menudesc">
-              {/* Dummy content for 360 Enhancement */}
-              Dummy content for 360 Enhancement.
-            </div>
-            <div className="menudownload w-fit px-20 py-8 rounded-full">
-              Download App
-            </div>
-          </>
-        );
-      case "HDR Image":
-        return (
-          <>
-            <div className="menutitle">HDR Image</div>
-            <div className="menudesc">
-              {/* Dummy content for HDR Image */}
-              Dummy content for HDR Image.
-            </div>
-            <div className="menudownload w-fit px-20 py-8 rounded-full">
-              Download App
-            </div>
-          </>
-        );
-      case "Perspective correction":
-        return (
-          <>
-            <div className="menutitle">Perspective correction</div>
-            <div className="menudesc">
-              {/* Dummy content for Perspective correction */}
-              Dummy content for Perspective correction.
-            </div>
-            <div className="menudownload w-fit px-20 py-8 rounded-full">
-              Download App
-            </div>
-          </>
-        );
-      case "Image Relighting":
-        return (
-          <>
-            <div className="menutitle">Image Relighting</div>
-            <div className="menudesc">
-              {/* Dummy content for Image Relighting */}
-              Dummy content for Image Relighting.
-            </div>
-            <div className="menudownload w-fit px-20 py-8 rounded-full">
-              Download App
-            </div>
-          </>
-        );
-      case "Sales and Marketing":
-        return (
-          <>
-            <div className="menutitle">Sales and Marketing</div>
-            <div className="menudesc">
-              {/* Dummy content for Sales and Marketing */}
-              Dummy content for Sales and Marketing.
-            </div>
-            <div className="menudownload w-fit px-20 py-8 rounded-full">
-              Download App
-            </div>
-          </>
-        );
-      case "Raw and Jpg Format":
-        return (
-          <>
-            <div className="menutitle">Raw and Jpg Format</div>
-            <div className="menudesc">
-              {/* Dummy content for Raw and Jpg Format */}
-              Dummy content for Raw and Jpg Format.
-            </div>
-            <div className="menudownload w-fit px-20 py-8 rounded-full">
-              Download App
-            </div>
-          </>
-        );
-      case "Contrast and Brightness boost":
-        return (
-          <>
-            <div className="menutitle">Contrast and Brightness boost</div>
-            <div className="menudesc">
-              {/* Dummy content for Contrast and Brightness boost */}
-              Dummy content for Contrast and Brightness boost.
-            </div>
-            <div className="menudownload w-fit px-20 py-8 rounded-full">
-              Download App
-            </div>
-          </>
-        );
-      case "Lens correction":
-        return (
-          <>
-            <div className="menutitle">Lens correction</div>
-            <div className="menudesc">
-              {/* Dummy content for Lens correction */}
-              Dummy content for Lens correction.
-            </div>
-            <div className="menudownload w-fit px-20 py-8 rounded-full">
-              Download App
-            </div>
-          </>
-        );
-      // Add cases for other features if needed
-      default:
-        return null;
+          </div>
+        </>
+      );
     }
+    return null;
   };
   const transition = { duration: 0.5, ease: "easeOut" };
   return (
     <>
-      <div className="foicontainer">
+      <div className="foicontainer" id="diycamera">
         <motion.div
           initial={{ opacity: 0, y: -50 }}
           whileInView={{ opacity: 1, y: 0 }}
           transition={{ duration: 1.5, ease: "easeOut" }}
           viewport={{ once: true }}
           className="foitop"
+          id="diycamera"
         >
-          <div className="foititle">Features Of Instapix</div>
+          <div className="foititle">Features Of Listingpix</div>
           <div className="foidesc">
-            Completely synergize resource taxing relationships via premier niche
-            markets. Professionally cultivate one-to-one customer service with
-            service for state of the art customer service.
+            Our mobile app captures HDR images using the latest technology. Then
+            instantly merges and performs AI editing in the cloud to produce
+            stunning real estate images, instantly! Users can further adjust
+            these settings within the mobile app to fine tune the final image.
           </div>
         </motion.div>
         <div className="foimiddle">
@@ -182,12 +171,18 @@ const Featureofinstapix = () => {
           </div>
         </div>
         <div className="foibottom flex">
-          <div className="foibottomleft">{renderFeatureContent()}</div>
-          <div className="foibottomright">
-            {selectedFeature === "Sky replacement" && (
-              <img className="skyimgwidth" src={skyreplacement} alt="" />
-            )}
-          </div>
+          {" "}
+          <AnimatePresence mode="wait">
+            <motion.div
+              key={selectedFeature}
+              initial={{ opacity: 0, scale: 0.5 }}
+              animate={{ opacity: 1, scale: 1 }}
+              exit={{ opacity: 0, scale: 0.5 }}
+              transition={{ duration: 1, ease: "easeOut" }}
+            >
+              {renderFeatureContent()}
+            </motion.div>
+          </AnimatePresence>
         </div>
       </div>
     </>
@@ -195,3 +190,20 @@ const Featureofinstapix = () => {
 };
 
 export default Featureofinstapix;
+
+// import React from "react";
+// import ReactCompareImage from "react-compare-image";
+// import beforeimg from "../../Assets/Features Comparison Images/Sky Replace-Before.jpg";
+// import afterimage from "../../Assets/Features Comparison Images/Sky Replace-After.jpg";
+
+// const Featureofinstapix = () => {
+//   return (
+//     <>
+//       <div>
+//         <ReactCompareImage leftImage={beforeimg} rightImage={afterimage} />;
+//       </div>
+//     </>
+//   );
+// };
+
+// export default Featureofinstapix;
